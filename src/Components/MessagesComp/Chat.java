25d9740f9fdbe91f4edaeeaacc0a00f64a1480ff
ErenGokalp <aerengok123@gmail.com>
2024-05-12 20:54:06 +0300

    Drastically improved messages UI
    Looks a lot better now, although not perfect. Backend will be implemented as soon as the database gets added.

--- src/Components/MessagesComp/Chat.java
+++ src/Components/MessagesComp/Chat.java
@@ -4,28 +4,52 @@
  */
 package Components.MessagesComp;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JPanel;
+
 /**
  *
  * @author aeren
  */
-public class Chat extends javax.swing.JPanel {
+public class Chat extends JPanel {
 
-    /**
-     * Creates new form Chat
-     */
     public Chat (String name, String pfp, String message, MessagesPanel m) {
         initComponents();
         
         addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 
-                m.displayChat(new MessagePanel());
-                System.out.println("yeet");
+                m.displayChat(new MessagePanel(m));
                 
             }
         });
         
-        jLabel1.setText(name);
+        jLabel3.setText(name);
+        repaint();
+    }
+    
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g.create();
+        
+        int width = getWidth();
+        int height = getHeight();
+        int arc = 30;
+        
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setColor(new Color(0, 0, 0, 40));
+        g2d.fill(new RoundRectangle2D.Double(2, 2, width, height, arc, arc));
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2d.setColor(getBackground());
+        g2d.fill(new RoundRectangle2D.Double(0, 0, width - 1, height - 1, arc, arc));
+
+        g2d.dispose();
     }
     
     
@@ -39,46 +63,49 @@ public class Chat extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
+        imageAvatar1 = new Components.ImageAvatar();
 
-        jLabel1.setText("PROFILE_PIC");
+        setBackground(new java.awt.Color(255, 255, 255));
+        setOpaque(false);
 
         jLabel2.setText("LAST_SENT_MESSAGE");
 
         jLabel3.setText("NAME");
+
+        imageAvatar1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icons/profilePic.png"))); // NOI18N
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel2)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addGap(31, 31, 31)
-                        .addComponent(jLabel3)))
-                .addContainerGap(785, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(imageAvatar1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 511, Short.MAX_VALUE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(277, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel3))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 65, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(39, 39, 39))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, 90, Short.MAX_VALUE))
+                    .addComponent(imageAvatar1, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private Components.ImageAvatar imageAvatar1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
--- src/Components/MessagesComp/MessagesPanel.java
+++ src/Components/MessagesComp/MessagesPanel.java
@@ -5,11 +5,13 @@
 package Components.MessagesComp;
 
 import Components.RefreshablePanel;
-import java.awt.Color;
-import java.awt.Dimension;
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
+import javax.swing.JScrollPane;
+import Components.ScrollBarUI;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
 /**
  *
@@ -22,39 +24,58 @@ public class MessagesPanel extends javax.swing.JPanel implements RefreshablePane
      */
     public MessagesPanel() {
         initComponents();
-        Chats.setBorder(BorderFactory.createEmptyBorder(20, 10, 10, 10));
-        Chats.setVisible(true);
-        Chats.setLayout(new BoxLayout(Chats, BoxLayout.Y_AXIS));
-        addChat ("Eren");
+        Chats.setLayout(new GridBagLayout());
+        Chats.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        ChatsScrollPane.setBorder(null);
+        ChatsScrollPane.setViewportView(Chats);
+        ChatsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        ChatsScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        ChatsScrollPane.getVerticalScrollBar().setUI(new ScrollBarUI());
+        ChatsScrollPane.getViewport().setBackground(new Color(255, 255, 255));
+        
+        addChat ("Eren");   
+        addChat ("Atahan");
+        addChat ("Emir");
+        addChat ("Deniz");
+        addChat ("Zuhal");
+        
     }
     
     public void refresh(){
         //TODO
     }
     
-    public static void main(String[] args) {
-        JFrame test = new JFrame();
-        test.add (new MessagesPanel());
-        test.pack();
-        test.setVisible(true);
-        test.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-    
-    //For testing purposes
     private void addChat (String name) {
         Chat c = new Chat (name, "e", "e", this);
-        c.setMaximumSize (new Dimension (942, 171));
-        c.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        Chats.add (c);
+        
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 5, 5);
+        
+        Chats.add(c, gbc);
         Chats.revalidate();
     }
     
     public void displayChat (MessagePanel m) {
         Chats.removeAll();
-        m.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-        m.setVisible(true);
-        Chats.add(m);
-        Chats.revalidate();
+        if (m != null) {
+            m.setBorder(BorderFactory.createLineBorder(null));
+            m.setVisible(true);
+            Chats.add(m);
+            ChatsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        } else {
+            addChat("Eren");
+            addChat("Atahan");
+            addChat("Emir");
+            addChat("Deniz");
+            addChat("Zuhal");
+            ChatsScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        }
+        
+        Chats.revalidate();  
     }
 
     /**
@@ -67,22 +88,29 @@ public class MessagesPanel extends javax.swing.JPanel implements RefreshablePane
     private void initComponents() {
 
         ChatsScrollPane = new javax.swing.JScrollPane();
-        Chats = new javax.swing.JPanel();
+        Chats = new Components.RoundedPanel();
 
         setBackground(new java.awt.Color(255, 204, 204));
-        setMaximumSize(new java.awt.Dimension(1000, 800));
-        setMinimumSize(new java.awt.Dimension(1000, 800));
-        setPreferredSize(new java.awt.Dimension(1000, 800));
+        setMaximumSize(new java.awt.Dimension(1150, 800));
+        setMinimumSize(new java.awt.Dimension(1150, 800));
+        setPreferredSize(new java.awt.Dimension(1150, 800));
+
+        ChatsScrollPane.setBackground(new java.awt.Color(255, 255, 255));
+        ChatsScrollPane.setBorder(null);
+
+        Chats.setBackground(new java.awt.Color(255, 255, 255));
+        Chats.setMaximumSize(new java.awt.Dimension(1050, 700));
+        Chats.setMinimumSize(new java.awt.Dimension(1050, 700));
 
         javax.swing.GroupLayout ChatsLayout = new javax.swing.GroupLayout(Chats);
         Chats.setLayout(ChatsLayout);
         ChatsLayout.setHorizontalGroup(
             ChatsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 986, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         ChatsLayout.setVerticalGroup(
             ChatsLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 786, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
 
         ChatsScrollPane.setViewportView(Chats);
@@ -92,22 +120,22 @@ public class MessagesPanel extends javax.swing.JPanel implements RefreshablePane
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(ChatsScrollPane)
-                .addContainerGap())
+                .addGap(67, 67, 67)
+                .addComponent(ChatsScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 1009, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(67, 67, 67))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(ChatsScrollPane)
-                .addContainerGap())
+                .addGap(50, 50, 50)
+                .addComponent(ChatsScrollPane, javax.swing.GroupLayout.PREFERRED_SIZE, 700, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(50, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel Chats;
+    private Components.RoundedPanel Chats;
     private javax.swing.JScrollPane ChatsScrollPane;
     // End of variables declaration//GEN-END:variables
 }
