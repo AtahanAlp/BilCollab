685ffda0f15a6d9b5b59c7d07ba037fd8d0ed829
Zuhal Aksoy <zuhalaksoy04@gmail.com>
2024-05-17 14:31:23 +0300

    Join button set user to current user

--- src/Components/ActivitiesComp/ActivityItem.java
+++ src/Components/ActivitiesComp/ActivityItem.java
@@ -232,7 +232,7 @@ public class ActivityItem extends javax.swing.JPanel {
     private void joinBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_joinBtnActionPerformed
         User currentUser = getCurrentUser();
         
-        if (activity.getParticipants().contains(user)) 
+        if (activity.getParticipants().contains(currentUser)) 
         {
             joinBtn.setText("ALREADY JOINED");
             joinBtn.setEnabled(false);
@@ -240,12 +240,12 @@ public class ActivityItem extends javax.swing.JPanel {
         
         }
         else if (activity.getParticipants().size() < activity.getQuota()) {
-            activity.joinUser(user);
+            activity.joinUser(currentUser);
             setQuotaDisplay();
 
             joinBtn.setText("JOINED");
             joinBtn.setEnabled(false);
-            activity.addParticipants(user.getId());
+            activity.addParticipants(currentUser.getId());
         }
         else
         {
